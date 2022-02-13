3c8b760f307966f5b66dd237ca0c42e817c7badf
Baiborin Petr <razor267@mail.ru>
2022-02-13 18:44:45 +0300

    animation CSS. LES 124

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-// const dist = "./dist/";
-const dist = "D:/Program/OpenServer/domains/irvas";
+const dist = "./dist/";
+// const dist = "D:/Program/OpenServer/domains/irvas";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
